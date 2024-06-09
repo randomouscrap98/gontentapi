5a82b9e9388ebb7f6fece47190ee7ae06e684fb8
Carlos Sanchez <randomouscrap98@aol.com>
2024-06-09 17:38:46 -0400

    More config

--- config.go
+++ config.go
@@ -13,6 +13,9 @@ type Config struct {
 	StaticFiles  string         // Where the static files for ALL endpoints go
 	HeaderLimit  int            // Maximum allowed header size
 	Timeout      utils.Duration // How long a connection is allowed to last
+	Database     string         // Path to the contentapi database file
+	Uploads      string         // Path to all the uploaded files
+	Templates    string         // Path to all the templates
 }
 
 func GetDefaultConfig_Toml() string {
@@ -22,6 +25,9 @@ ShutdownTime="10s"            # How long to wait for the server to shutdown
 StaticFiles="static"          # Where ALL static files go
 HeaderLimit=10000             # Maximum allowed header size on POST
 Timeout="30s"                 # How long a connection is allowed to last
+Database="data/content.db"    # Path to the contentapi database file
+Uploads="data/uploads"        # Path to the contentapi uploads (images)
+Templates="static/templates"  # Path to all the templates
 `
 	return fmt.Sprintf(
 		baseConfig,
